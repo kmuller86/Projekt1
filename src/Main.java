66cea3fed16525d735cdea60582026a0b3d0c000
krzys <krzysztofmuller1@gmail.com>
2021-08-28 21:36:35 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Witaj świecie");
+        System.out.println("Po jest nasz pierwszy program w języku java");
+        System.out.println("Życzę" + " " + "wam " + "miłej nauki!");
+        System.out.printf("A %s zaczynamy z %d przykładem.", "więc", 1);
+    }
+}
